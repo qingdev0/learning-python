0c0165d4f9989e69b1d37f92c0b36f75f5e989fc
Ewan Li <ewan.li@icloud.com>
2025-01-18 20:24:10 +0800

    feat: add Python learning resources and logging examples
    - Introduced a Justfile for managing Python project tasks.
    - Updated requirements.txt to specify versions for matplotlib, numpy, and added pylint.
    - Added custom dictionary for spell checking in Python files.
    - Configured Dependabot for weekly updates on pip dependencies.
    - Enhanced logging examples with detailed comments and structured logging.
    - Created a new plot script to visualize time complexity functions.

--- logging/opening_03.py
+++ logging/opening_03.py
@@ -1,30 +1,38 @@
+"""learn logging 3/3"""
 import logging
 
 
 def get_logger():
-    # Create logger
-    logger = logging.getLogger("arjancodes")
-
-    # Create handler
-    handler = logging.StreamHandler()
+    """advanced usage:
+    logger, formatter and handler:
+      1) create formatter - formatter.datefmt
+      2) create handler - handler.setFormatter(formatter)
+      3) create logger - logger.setLevel; logger.addHandler(handler)
+    """
+    # Create logger with the name "my_logger"
+    logger = logging.getLogger("my_logger")
+    logger.setLevel(logging.INFO)  # Set log level
 
     # Create formatter
-    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     formatter.datefmt = "%Y-%m-%d %H:%M:%S"
 
-    # Set formatter on handler
+    # Create handler, and set formatter on handler
+    handler = logging.StreamHandler()
     handler.setFormatter(formatter)
-
-    # Add handler to logger
     logger.addHandler(handler)
 
-    # Set log level
-    logger.setLevel(logging.INFO)
+    # Create a file handler and set its level
+    file_handler = logging.FileHandler("logging/opening_03.log")
+    file_handler.setLevel(logging.WARNING)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
     return logger
 
 
 def main():
+    """main"""
     logger = get_logger()
 
     logger.debug("This is a debug message.")
